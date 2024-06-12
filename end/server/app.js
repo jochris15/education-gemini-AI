2c432722b7ef08b15c2022d8f2b8979905484955
jochris15 <jochris15@gmail.com>
2024-06-12 15:08:23 +0700

    test

--- end/server/app.js
+++ end/server/app.js
@@ -10,24 +10,30 @@ app.use(express.json())
 
 app.get('/popular-pokemon', async (req, res, next) => {
     try {
-        const vertexAI = new VertexAI({ project: "bsd-15", location: 'asia-southeast1' });
+        const vertexAI = new VertexAI({ project: "bsd-15", location: 'us-central1' });
 
         const generativeModel = vertexAI.getGenerativeModel({
             model: 'gemini-1.5-flash-001',
         });
 
         const prompt =
-            "Please give me a name of the most popular pokemon";
+            "Please give me only a name for a popular pokemon without bolding the text";
 
-        // const resp = await generativeModel.generateContent(prompt);
-        // const contentResponse = await resp.response;
-        // console.log(JSON.stringify(contentResponse));
+        const resp = await generativeModel.generateContent(prompt);
+        const contentResponse = await resp.response;
 
-        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+        console.log(JSON.stringify(contentResponse));
+        console.log(contentResponse?.candidates[0]?.content?.parts[0]?.text.toString().toLowerCase());
+
+        const pokemonName = contentResponse?.candidates[0]?.content?.parts[0]?.text.toString().toLowerCase() || 'pikachu'
+
+        const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
         res.status(200).json(data)
     } catch (error) {
-        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        })
     }
 })
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+# GEMINI AI (VERTEX AI)
+
+## SETUP
+[Dokumentasi SETUP GEMINI AI](https://cloud.google.com/vertex-ai/generative-ai/docs/start/quickstarts/quickstart-multimodal#local-shell)
